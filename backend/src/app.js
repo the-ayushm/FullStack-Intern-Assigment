ab70330e7eea7ba32208bd1bde030812de6a2161
soft7 <ayushm1247@gmail.com>
2026-04-10 00:34:38 +0530

    fixed cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: true,
-    credentials: false,
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true,
   })
 );
 app.use(express.json({ limit: '1mb' }));
